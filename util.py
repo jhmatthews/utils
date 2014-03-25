6a07b7832635105e0a519f830c4e4dbd8f879559
jhmatthews <jm8g08@cyan01.(none)>
2014-03-25 16:21:46 +0000

    fixed util.py

--- util.py
+++ util.py
@@ -1,9 +1,3 @@
-/Library/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python
-/Library/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python
-py_ex
-py_ex
-/Library/Frameworks/Python.framework/Versions/2.7/Resources/Python.app/Contents/MacOS/Python
-#/Library/Frameworks/Python.framework/Versions/2.7/bin/python
 '''
 MEGA USEFUL GENERAL PURPOSE STUFF
 '''
@@ -12,27 +6,6 @@ def strip(character, string):
     ''' strip a character from a string'''
     new_string = ""
     for s in string:
-        if s != character:
-            new_string += s
-    return new_stringf strip(character, string):
-    ''' strip a character from a string'''
-    new_string = ""
-    for s in string:
-        if s != character:
-            new_string += s
-    return new_string for s in string:
-        if s != character:
-            new_string += s
-    return new_string for s in string:
-        if s != character:
-            new_string += s
-    return new_string for s in string:
-        if s != character:
-            new_string += s
-    return new_stringg for s in string:
-        if s != character:
-            new_string += s
-    return new_stringg for s in string:
         if s != character:
             new_string += s
     return new_string
